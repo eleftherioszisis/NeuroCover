652fb163dcd9ca7cab21f4ebfe5550a4a25d9bea
Lida Kanari <lida.kanari@epfl.ch>
2018-01-18 13:52:30 +0100

    Automatic pip installation and README file

--- setup.py
+++ setup.py
@@ -6,13 +6,34 @@ import glob
 
 from pip.req import parse_requirements
 from setuptools import find_packages, setup, Extension
-
+from optparse import Option
+import pip
 
 NAME='neurocover'
 
 
 pwd = os.path.dirname(__file__)
 
+def parse_reqs(reqs_file):
+    ''' parse the requirements '''
+    options = Option('--workaround')
+    options.skip_requirements_regex = None
+    # Hack for old pip versions
+    # Versions greater than 1.x have a required parameter "sessions" in
+    # parse_requierements
+    if pip.__version__.startswith('1.'):
+        install_reqs = parse_requirements(reqs_file, options=options)
+    else:
+        from pip.download import PipSession  # pylint:disable=E0611
+        options.isolated_mode = False
+        install_reqs = parse_requirements(reqs_file,  # pylint:disable=E1123
+                                          options=options,
+                                          session=PipSession)
+
+    return [str(ir.req) for ir in install_reqs]
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+REQS = parse_reqs(os.path.join(BASEDIR, 'requirements.txt'))
 
 def scandir(directory, files=[]):
 
@@ -37,6 +58,7 @@ setup(
       description='Neruon Cover scripts',
       author='Eleftherios Zisis',
       author_email='eleftherios.zisis@epfl.ch',
+      install_requires= REQS,
       packages=find_packages(),
       scripts=['apps/create_covering_animation.py'],
       include_package_data=True,
